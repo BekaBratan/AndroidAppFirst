32e6b06d8ef8dbba4a805afbf41a7be72240b11f
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2023-10-15 00:44:43 +0600

    Changing all structure using code in designing

--- app/src/main/java/com/example/firstapplicationwithgit/MainActivity.kt
+++ app/src/main/java/com/example/firstapplicationwithgit/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.firstapplicationwithgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter.LengthFilter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if (login == "" || email == "" || pass == "")
+                Toast.makeText(this, "Not all plains are filled", Toast.LENGTH_SHORT).show()
+        }
     }
 }
