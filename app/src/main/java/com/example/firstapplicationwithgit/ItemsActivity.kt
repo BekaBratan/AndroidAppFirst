cd14b039afd62a491052146af939f1e1e7271f7f
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2023-11-01 17:20:35 +0600

    add function for adding goods in items list

--- app/src/main/java/com/example/firstapplicationwithgit/ItemsActivity.kt
+++ app/src/main/java/com/example/firstapplicationwithgit/ItemsActivity.kt
@@ -6,20 +6,25 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
+
+    private val items = arrayListOf<Item>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
 
         val itemsList: RecyclerView = findViewById(R.id.items_list)
-        val items = arrayListOf<Item>()
 
+        listItems()
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemsAdapter(items, this)
+    }
+
+    private fun listItems() {
         items.add(Item(1, "burger", "Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 300))
         items.add(Item(2, "taco", "Taco", "Lorem ipsum dolor sit amet, consectetur adipiscing elit","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 350))
         items.add(Item(3, "chicken", "Chicken", "Lorem ipsum dolor sit amet, consectetur adipiscing elit","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 400))
         items.add(Item(4, "cola", "Cola", "Lorem ipsum dolor sit amet, consectetur adipiscing elit","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 250))
         items.add(Item(5, "water", "Water", "Lorem ipsum dolor sit amet, consectetur adipiscing elit","Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 100))
-
-        itemsList.layoutManager = LinearLayoutManager(this)
-        itemsList.adapter = ItemsAdapter(items, this)
     }
 }
